085638146e5244d38f56bbeaa53f78af5687b748
Sumit <sumitkumar280601@gmail.com>
2024-08-15 22:27:15 +0530

    Add mv package

--- controller/UploadHandle.js
+++ controller/UploadHandle.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const fs = require("fs");
 const { uploadFileLog } = require('../extra/Logging')
+const mv = require('mv');
 
 
 const handleError = (err, res) => {
@@ -19,7 +20,7 @@ const UploadHandle = (req, res) => {
   let originalname = req.file.originalname
   let fakename = btoa(encodeURIComponent(originalname));
   const targetPath = path.join(__dirname, `../tmp/resource/${fakename}`);
-  fs.rename(tempPath, targetPath, err => {
+  mv(tempPath, targetPath, {mkdirp: true},  err => {
     if (err) return handleError(err, res);
     res
       .status(200)
